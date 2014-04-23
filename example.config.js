1247d499040e61d00729d549715ff3be8707e527
Jwaxo <jeff@jeffwax.com>
2014-04-23 14:16:37 -0700

    Added example config file.

---
+++ example.config.js
@@ -0,0 +1,19 @@
+var config = {}
+
+config.handle = 'osbot'
+
+config.network = 'irc.freenode.net'
+
+config.params = {
+  channels: ['#examplechannel']
+}
+
+config.github = {
+  user: 'your user',
+  email: 'your user email',
+  password: 'password',
+  repo: 'the repo of your bot',
+  token: 'your user token'
+}
+
+module.exports = config
