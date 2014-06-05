b4a7961373dedd2ea34cf0a4c517cff27ef7b633
Jwaxo <jeff@jeffwax.com>
2014-06-04 22:45:43 -0500

    Removed 'onMessage' function groupings.

--- index.js
+++ index.js
@@ -117,12 +117,10 @@ var builtins = [
       }
     ],
     run: {
-      onmessage: {
-        reload: function (client, message) {
-          functions.loadPlugins();
-          client.speak(message, 'Reloaded definition files.');
-          return {status:"success"};
-        }
+      reload: function (client, message) {
+        functions.loadPlugins();
+        client.speak(message, 'Reloaded definition files.');
+        return {status:"success"};
       }
     }
   },
@@ -139,59 +137,57 @@ var builtins = [
       }
     ],
     run: {
-      onmessage: {
-        help: function (client, message, args) {
-          var command;
-          var allbuiltinnames = builtins.map(function (elem) {
-            if (elem.hasOwnProperty('help')) {
-              return elem.name;
-            }
-          }).filter(function(value) {
-            return value !== false;
-          }).join(", ")
-            , allpluginnames = plugins.map(function (elem) {
-            if (elem.hasOwnProperty('help')) {
-              return elem.name;
-            }
-            else {
-              return false;
-            }
-          }).filter(function (value) {
-            return value !== false;
-          }).join(", ");
-          var helpfunctions;
-
-          if (args[0]) {
-            command = args[0];
+      help: function (client, message, args) {
+        var command;
+        var allbuiltinnames = builtins.map(function (elem) {
+          if (elem.hasOwnProperty('help')) {
+            return elem.name;
           }
-
-          if (command) {
-            builtins.forEach(function (builtin) {
-              if (builtin.name === command && builtin.hasOwnProperty('help')) {
-                helpfunctions = builtin.help;
-              }
-            });
-            if (!helpfunctions) {
-              plugins.forEach(function (plugin) {
-                if (plugin.name === command && plugin.hasOwnProperty('help')) {
-                  helpfunctions = plugin.help;
-                }
-              });
-            }
-            if (helpfunctions) {
-              helpfunctions.forEach(function (helpfunction) {
-                client.speak(message, 'Usage: ' + helpfunction.usage + ', ' + helpfunction.description);
-              });
-            }
-            else {
-              client.speak(message, 'Sorry, function not found or has no help info.');
-            }
+        }).filter(function(value) {
+          return value !== false;
+        }).join(", ")
+          , allpluginnames = plugins.map(function (elem) {
+          if (elem.hasOwnProperty('help')) {
+            return elem.name;
           }
           else {
-            client.speak(message, 'Possible help topics: ' + allbuiltinnames + ', ' + allpluginnames);
+            return false;
           }
-          return {status:"success"};
+        }).filter(function (value) {
+          return value !== false;
+        }).join(", ");
+        var helpfunctions;
+
+        if (args[0]) {
+          command = args[0];
         }
+
+        if (command) {
+          builtins.forEach(function (builtin) {
+            if (builtin.name === command && builtin.hasOwnProperty('help')) {
+              helpfunctions = builtin.help;
+            }
+          });
+          if (!helpfunctions) {
+            plugins.forEach(function (plugin) {
+              if (plugin.name === command && plugin.hasOwnProperty('help')) {
+                helpfunctions = plugin.help;
+              }
+            });
+          }
+          if (helpfunctions) {
+            helpfunctions.forEach(function (helpfunction) {
+              client.speak(message, 'Usage: ' + helpfunction.usage + ', ' + helpfunction.description);
+            });
+          }
+          else {
+            client.speak(message, 'Sorry, function not found or has no help info.');
+          }
+        }
+        else {
+          client.speak(message, 'Possible help topics: ' + allbuiltinnames + ', ' + allpluginnames);
+        }
+        return {status:"success"};
       }
     }
   }
@@ -201,8 +197,8 @@ var checkCommand = function (plugin, event, command, message, args) {
   var result = {
     status: 'fail'
   };
-  if (plugin.run.hasOwnProperty(event) && plugin.run[event].hasOwnProperty(command)) {
-    result = plugin.run[event][command](client, message, args, requires);
+  if (plugin.hasOwnProperty(event) && plugin[event].hasOwnProperty(command)) {
+    result = plugin[event][command](client, message, args, requires);
   }
   return result;
 };
@@ -219,7 +215,11 @@ client.addListener('error', function (content) {
 client.addListener('message', function (from, to, content) {
   var builtin_found = false,
       plugin_found = false,
-      input = /^!(\S+)(.*)$/.exec(content),
+      message_groups = {
+        run: /^!(\S+)(.*)$/,
+        group: /^@(\S+)(.*)$/ // Ideally this could be set up by the group plugin,
+        // but there is no infrastructure for it at the moment, thus it is hard-defined
+      },
       args = [],
       command;
   var message = {
@@ -229,44 +229,45 @@ client.addListener('message', function (from, to, content) {
   };
   console.log(from + ' said ' + content + ' to ' + to);
 
-  if (input) {
+  for (var event in message_groups) {
+    var input = message_groups[event].exec(content);
+    if (input) {
 
-    if (input[2]) {
-      args = input[2].trim().split([' ']);
-    }
-    if (input[1]) {
-      command = input[1];
-      builtin_found = builtins.some(function (builtin) {
-        if (checkCommand(builtin, 'onmessage', command, message, args).status === 'success') {
-          return true;
-        }
-        return false;
-      });
-
-      if (!builtin_found) {
-        plugin_found = plugins.some(function (plugin) {
-          var result = checkCommand(plugin, 'onmessage', command, message, args);
-
-          switch (result.status) {
-            case 'fail':
-              break
-            case 'update':
-              if (result.hasOwnProperty('file') && result.hasOwnProperty('data')) {
-                functions.updateFile(result.file, result.data);
-                console.log(result.file + ' updated.');
-              } else {
-                console.log('Missing update file or data.');
-              }
-            case 'success':
-              return true;
+      if (input[2]) {
+        args = input[2].trim().split([' ']);
+      }
+      if (input[1]) {
+        command = input[1];
+        builtin_found = builtins.some(function (builtin) {
+          if (checkCommand(builtin, event, command, message, args).status === 'success') {
+            return true;
           }
           return false;
         });
+
+        if (!builtin_found) {
+          plugin_found = plugins.some(function (plugin) {
+            var result = checkCommand(plugin, 'onmessage', command, message, args);
+
+            switch (result.status) {
+              case 'fail':
+                break
+              case 'update':
+                if (result.hasOwnProperty('file') && result.hasOwnProperty('data')) {
+                  functions.updateFile(result.file, result.data);
+                  console.log(result.file + ' updated.');
+                } else {
+                  console.log('Missing update file or data.');
+                }
+              case 'success':
+                return true;
+            }
+            return false;
+          });
+        }
       }
     }
-    return true;
   }
-  return false;
 });
 
 // When a user joins the channel, look through all plugins and builtins for an
